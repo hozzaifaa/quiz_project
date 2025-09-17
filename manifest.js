15d657fdd456265d8c152f4a3399f17287ea748d
hozzaifaa <hozzaifaa@gmail.com>
2025-09-17 17:52:57 +0200

    new updates

---
+++ manifest.js
@@ -0,0 +1,7 @@
+// manifest.js
+window.quizManifest = [
+  { file: "quiz1.js", title: "Quiz 1" },
+  { file: "quiz2.js", title: "Quiz 2" },
+  { file: "quiz3.js", title: "Quiz 3" }
+  // Add more here whenever you add a new quiz
+];
